21dd185d2e8a722407e54dbe986645572004b88f
Joakin21 <1karloz.96@gmail.com>
2020-08-08 11:31:38 -0300

    Version inicial

---
+++ src/components/MyTable.vue
@@ -0,0 +1,37 @@
+<template>
+  
+  <div>
+    <h3 class="mb-3 text-center">{{title_mytable}}</h3>
+
+    <b-table striped hover :items="items"></b-table>
+      
+  </div>
+ 
+</template>
+
+<script>
+export default {
+  name:"MyTable",
+  data() {
+    return {
+      title_mytable:"Alumnos",
+      items: [
+          {age:23, first_name:"Carlos", last_name:"Navarro"},
+          {age:21, first_name:"Larsen", last_name:"Shaw"},
+          {age:89, first_name:"Geneva", last_name:"Wilson"},
+          {age:12, first_name:"Jami", last_name:"Carney"},
+      ]
+    }
+  },
+  methods: {
+    saludar: function(){
+      alert("Hola :)")
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+
+</style>
